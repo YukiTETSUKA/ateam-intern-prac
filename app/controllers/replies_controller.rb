934d6cffd096d35e449c8433b320bfb526dc4e36
Yuki Tetsuka <yuki@Yuki-no-MacBook-Air.local>
2016-11-29 23:50:59 +0900

    replyのコントローラ，ルーティングなどを追加

---
+++ app/controllers/replies_controller.rb
@@ -0,0 +1,30 @@
+class RepliesController < ApplicationController
+  before_action :set_reply, only: [:show, :edit, :update, :destroy]
+
+  # POST /replies
+  def create
+    @reply = Reply.new(reply_params)
+
+    respond_to do |format|
+      if @reply.save
+        format.html { redirect_to @reply, notice: 'Reply was successfully created.' }
+        format.json { render :show, status: :created, location: @reply }
+      else
+        format.html { render :new }
+        format.json { render json: @reply.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_reply
+      @reply = Reply.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def reply_params
+      params.fetch(:reply, {})
+      params.require(:reply).permit(:blog_id, :comment)
+    end
+end
