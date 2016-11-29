5ff390d5c66a97ce23be2d777244ebc53f2eb912
Yuki Tetsuka <yuki@Yuki-no-MacBook-Air.local>
2016-11-29 23:35:01 +0900

    余分な行の削除，いいねの実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,62 +2,31 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
   # GET /blogs
-  # GET /blogs.json
   def index
     @blog = Blog.new
   end
 
   # GET /blogs/1
-  # GET /blogs/1.json
   def show
   end
 
-  # GET /blogs/new
-  def new
-    @blog = Blog.new
-  end
-
-  # GET /blogs/1/edit
-  def edit
-  end
-
   # POST /blogs
-  # POST /blogs.json
   def create
     @blog = Blog.new(blog_params)
 
     respond_to do |format|
-      if @blog.save
-        format.html { redirect_to :root, notice: 'Blog was successfully created.' }
-        format.json { render :show, status: :created, location: @blog }
-      else
-        format.html { render :new }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
-      end
+      message = @blog.save ? 'Blog was successfully created.' : 'failed.'
+      format.html { redirect_to :root, notice: message }
     end
   end
 
-  # PATCH/PUT /blogs/1
-  # PATCH/PUT /blogs/1.json
-  def update
-    respond_to do |format|
-      if @blog.update(blog_params)
-        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-        format.json { render :show, status: :ok, location: @blog }
-      else
-        format.html { render :edit }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # POST /likes/1
+  def like
+    blog = Blog.find(params[:blog_id])
 
-  # DELETE /blogs/1
-  # DELETE /blogs/1.json
-  def destroy
-    @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
-      format.json { head :no_content }
+      message = blog.increment(:like).save ? 'Success' : 'Failed'
+      format.html { redirect_to request.referer, notice: message }
     end
   end
 
@@ -70,6 +39,6 @@ class BlogsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
       params.fetch(:blog, {})
-      params.require(:blog).permit(:body)
+      params.require(:blog).permit(:body, :like)
     end
 end
